7435607b2d58b095edaf788508283674721b7ad8
Zac H <zac.haluza@gmail.com>
2020-02-08 23:08:57 -0500

    added playlist exercise

---
+++ Exercises/playlist.py
@@ -0,0 +1,13 @@
+playlist = {
+    'name': 'Random Playlist',
+    'creator': 'Zac',
+    'songs': [
+        {'title': 'Nyu',
+         'artist': 'Veil of Maya',
+         'album': 'Matriarch'
+         },
+        {'title': 'Flying Whales',
+         'artist': 'Gojira',
+         'album': 'From Mars to Sirius'}
+    ]
+}
