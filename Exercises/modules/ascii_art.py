1b3261ab52bfbc2243205897e84b6accc9c70c30
Zac H <zac.haluza@gmail.com>
2020-12-01 21:40:20 -0500

    format with autopep8

--- Exercises/modules/ascii_art.py
+++ Exercises/modules/ascii_art.py
@@ -1,16 +1,19 @@
 from pyfiglet import figlet_format
 from termcolor import colored
 
-def print_ascii_art(msg, color): 
+
+def print_ascii_art(msg, color):
     msg = figlet_format(msg)
 
-    valid_colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+    valid_colors = ['grey', 'red', 'green',
+                    'yellow', 'blue', 'magenta', 'cyan', 'white']
     if color == 'gray':
         color = 'grey'
     if(color not in valid_colors):
         color = 'green'
 
     print(colored(msg, color=color))
+
 
 # Gather user input
 message = input('What message do you want to print?')
